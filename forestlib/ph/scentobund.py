722a31b8244e7b95718798a9a722ca0e4e9b3fa1
Rachael May Alfant <rmalfan@sisu3.sandia.gov>
2024-09-23 14:52:31 -0600

    added bundle_scheme and scenlist_json

--- forestlib/ph/scentobund.py
+++ forestlib/ph/scentobund.py
@@ -1,12 +1,36 @@
-import json
-with open('mpisppy/agnostic/scenlist.json', 'r') as file:
-    data = json.load(file)
+# function that returns scenario data from json file - may add additional file formats later
+def scenlist_json():
+    import json
+    with open('scenlist.json', 'r') as file:
+        data = json.load(file)
+    
+    return data
 
+data = scenlist_json()  # scenario data
+scheme = 0   # determines which bundling scheme to use (will probably change how we do this later)
+
+# if-else in this file so we don't have to change class StochasticProgram
+def bundle_scheme(data, scheme): # bare bones; will change as we add schema
+    if scheme == 0:
+        return bundle_by_fidelity(data)
+    elif scheme == 1:
+        return bundle_multifid(data)
+    else:
+        return some_other_bundling_scheme(data)
+    
+'''
+bundle is a dictionary of dictionaries
+    - keys are names of bundles
+    - for each dictionary in bundle, keys are 'IDs' (i.e., which scenarios are in the bundle) and 'Probability'
+'''
 bundle = {}
 
 def bundle_by_fidelity(data):
-    bundle['HF'] = {}
-    bundle['LF'] = {}
+
+    bundle_names = ['HF', 'LF']
+    for bund in bundle_names:
+        bundle[bund] = {}
+
     scenID_HFlist = []
     scenID_LFlist = []
     for i in range(len(data['scenarios'])):
@@ -15,24 +39,29 @@ def bundle_by_fidelity(data):
         elif data['scenarios'][i]['Fidelity'] == 'LF':
             scenID_LFlist.append(data['scenarios'][i]['ID'])
         else:
-            raise RuntimeError (f"No fidelity specified for {i}th scenario") # deal with order later
-
-    # will sum to 1 b/c uniform distribution - fix later with the rule from the escudero paper   
+            raise RuntimeError (f"No fidelity specified for {i}th scenario")
+    # will sum to 1 b/c uniform distribution - will fix later with the rule from the escudero paper
     bundle['HF'] = {'IDs':         scenID_HFlist,
-                    'Probability': sum(data['scenarios'][scenID_HFlist.index(j)]['Probability'] for j in scenID_HFlist),
-                    'Demand':      sum(data['scenarios'][scenID_HFlist.index(j)]['Demand'] for j in scenID_HFlist)/len(scenID_HFlist)}
+                    'Probability': sum(data['scenarios'][scenID_HFlist.index(j)]['Probability'] for j in scenID_HFlist)}
     bundle['LF'] = {'IDs':         scenID_LFlist,
-                    'Probability': sum(data['scenarios'][scenID_LFlist.index(j)]['Probability'] for j in scenID_LFlist),
-                    'Demand':      sum(data['scenarios'][scenID_LFlist.index(j)]['Demand'] for j in scenID_LFlist)/len(scenID_LFlist)}
+                    'Probability': sum(data['scenarios'][scenID_LFlist.index(j)]['Probability'] for j in scenID_LFlist)}
 
     return bundle
 
 
-def bundle_multifid(data):
+def bundle_multifid(data):  # still needs some work (don't use yet!)
+    bundle_names = ['Low', 'Medium', 'High']
+
+    for bund in bundle_names:
+        bundle[bund] = {}
+
+    bundle['Low']    = {'IDs': [0, 1, 6],    'Probability': 0.2}
+    bundle['Medium'] = {'IDs': [2, 3, 7, 8], 'Probability': 0.5}
+    bundle['High']   = {'IDs': [4, 5, 9],    'Probability': 0.3}
+
+    return bundle
+
+
+def some_other_bundling_scheme(data):
     pass
-
-
-bundle_by_fidelity(data)
-
-print(bundle['LF'])
 
