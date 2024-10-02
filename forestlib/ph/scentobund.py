6d0ea8b323adf12f2fa44d440bff81d180709536
Rachael May Alfant <rmalfan@sisu3.sandia.gov>
2024-10-02 15:37:14 -0600

    fixed bundle_similar_partition

--- forestlib/ph/scentobund.py
+++ forestlib/ph/scentobund.py
@@ -65,41 +65,41 @@ def bundle_similar_partition(data): # bundle similar scenarios together; each sc
 
     list_scens = data['scenarios']
 
-    list_buns = {0: [[[0,1,2]]], 1: [[[0,1], [2]], [[0,2], [1]], [[1,2], [0]]], 2: [[[0]], [[1]], [[2]]]} ### need to think about how to generalize this
+    list_buns = {0: [[[0,1,2]]], 
+                 1: [[[0,1], [2]], [[0,2], [1]], [[1,2], [0]]], 
+                 2: [[[0]], [[1]], [[2]]]} ### need to think about how to generalize this
     bun_err = []
     for i in range(max_num_buns - min_num_buns + 1):
-        bun_mean = []
-        
-        # assign each scenario to closest bundle
-        for j in range(len(list_buns[i])):
+        bun_mean = []                       # initialize list of bundle means when the number of bundles is i
+        temp_bun_err = 0                    # initialize distance from each scenario to its assigned bundle when the number of bundles is i
+        for j in range(len(list_buns[i])):  # assign each scenario to closest bundle:
+            # compute mean of each bundle
             bun_mean.append([(sum(list_buns[i][j][m])/len(list_buns[i][j][m])) for m in range(len(list_buns[i][j]))])
-
-            temp_dict = {m: [] for m in range(len(list_buns[i][j]))}
-            temp_bun_err = []
-            #if all(q == bun_mean[0] for q in bun_mean): # need to add if statement to eliminate redundant bundle means / skip bundle reassignment if all bundle means are the same
-            for k, scen in enumerate(list_scens):
+            temp_dict = {m: [] for m in range(len(list_buns[i][j]))} # store each scenario's assignment to a bundle
+            
+            ### need to add if statement to eliminate redundant bundle means / skip bundle reassignment if all bundle means are the same #if all(q == bun_mean[0] for q in bun_mean): 
+            for k, scen in enumerate(list_scens): # scenario-bundle assignment based on min distance to bundle mean
                 dist_scen_to_bun = [abs(scen['Demand'] - bun_mean[j][m]) for m in range(len(list_buns[i][j]))]
                 min_dist = min(dist_scen_to_bun)
                 temp_dict[dist_scen_to_bun.index(min_dist)].append(scen)
-                temp_bun_err.append(abs(bun_mean[j][dist_scen_to_bun.index(min_dist)] - scen['Demand']))
+                temp_bun_err += (abs(bun_mean[j][dist_scen_to_bun.index(min_dist)] - scen['Demand']))
 
             # calculate bundling error 
-            bun_err.append(sum(temp_bun_err))
+        bun_err.append(temp_bun_err)
     
-    total_bun_err = [abs(bun_err[i] - bun_err[i-1]) for i in range(1, max_num_buns - min_num_buns + 2)]
+    total_bun_err = [abs(bun_err[i] - bun_err[i-1]) for i in range(1, max_num_buns - min_num_buns + 1)]
     max_total_bun_err = max(total_bun_err)
-    num_buns = total_bun_err.index(max_total_bun_err) + min_num_buns - 1
+    num_buns = total_bun_err.index(max_total_bun_err) + min_num_buns + 1
 
     '''
-    CREATE BUNDLES WITH num_buns
+    CREATE num_buns BUNDLES
     '''
     core_scenarios = {} # core scenarios are LF (can change depending on the problem)
     for i in range(len(data['scenarios'])):
         if data['scenarios'][i]['Fidelity'] == 'LF':
             core_scenarios.update({f'{data['scenarios'][i]['ID']}': data['scenarios'][i]['Demand']})
-            #core_scenarios.append(data['scenarios'][i])
 
-    # if num core_scens > num_buns, remove last scenario
+    # assert len(core_scenarios) == num_buns
 
     for s in core_scenarios.keys():
         bundle[s] = {'IDs': [], 'Probability': []}
@@ -109,7 +109,7 @@ def bundle_similar_partition(data): # bundle similar scenarios together; each sc
         if data['scenarios'][i]['Fidelity'] == 'HF':
             not_core_scenarios.update({f'{data['scenarios'][i]['ID']}': [data['scenarios'][i]['Demand'], data['scenarios'][i]['Probability']]})
 
-    dist_to_cores = [] #[ [] for k, scen in enumerate(list_scens)]
+    dist_to_cores = []
     for k, scen in enumerate(not_core_scenarios):
         for s, core in enumerate(core_scenarios.keys()):
             dist_to_cores.append(abs(not_core_scenarios[scen][0] - core_scenarios[core]))
@@ -118,10 +118,30 @@ def bundle_similar_partition(data): # bundle similar scenarios together; each sc
         closest_core = list(core_scenarios.items())[dist_to_cores.index(min_dist)][0]
         bundle[f'{closest_core}']['IDs'].append(scen)
         bundle[f'{closest_core}']['Probability'].append(not_core_scenarios[scen][1])
-        dist_to_cores = []
+        dist_to_cores = []  # reset dist_to_cores
         
-    # TEST THE FUNCTION AGAIN!!
-    # add if statement here to eliminate cores that have no associated scenarios
+    # eliminate cores that have no associated scenarios; append to nearest core 
+    uncore_scens = {}
+    for n, key in enumerate(core_scenarios):
+        if len(bundle[key]['IDs']) == 0:
+            uncore_scens.update({key: data['scenarios'][n]})
+            del bundle[key]
+
+    for key in uncore_scens:
+        del core_scenarios[key]
+
+    if len(uncore_scens) == 0:
+        pass
+    else:
+        for uc_scen in uncore_scens:
+            for s, core in enumerate(core_scenarios.keys()):
+                dist_to_cores.append(abs(uncore_scens[uc_scen]['Demand'] - core_scenarios[core]))
+            
+            min_dist = min(dist_to_cores)
+            closest_core = list(core_scenarios.items())[dist_to_cores.index(min_dist)][0]
+            bundle[f'{closest_core}']['IDs'].append(uc_scen)
+            bundle[f'{closest_core}']['Probability'].append(uncore_scens[uc_scen]['Probability'])
+            dist_to_cores = []  # reset dist_to_cores
 
     return bundle 
 
