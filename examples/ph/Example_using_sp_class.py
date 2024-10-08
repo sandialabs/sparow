02bdc7722d05ce14b00311523814f47950d0a54c
Zachary Alexander Kilwein <zakilwe@sisu3.sandia.gov>
2024-10-08 07:52:42 -0600

    Adapted example for single bundle scheme

--- examples/ph/Example_using_sp_class.py
+++ examples/ph/Example_using_sp_class.py
@@ -7,13 +7,13 @@ import os
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from forestlib.ph.sp import StochasticProgram_Pyomo
 
-def model_builder(scen):
-    M = pyo.ConcreteModel()
+def model_builder(scen,scen_args):
+    M = pyo.ConcreteModel(scen["ID"])
     M.x = pyo.Var()
     M.y = pyo.Var()
-    if scen=='good':
+    if scen["ID"]=='good':
         M.c=pyo.Constraint(expr=1*M.x**2==M.y)
-    elif scen=='bad':
+    elif scen["ID"]=='bad':
         M.c=pyo.Constraint(expr=(1*M.x+1)**2==M.y)
     M.obj=pyo.Objective(expr=M.y)
     return M
@@ -22,18 +22,36 @@ first_stage_vars=['x','y']
 scenarios=['good','bad']
 p={'good':0.5,'bad':0.5}
 
-S_EF=StochasticProgram_Pyomo(first_stage_variables=first_stage_vars,model_builder=model_builder)
-S_EF.pyo_solver=pyo.SolverFactory('ipopt')
+S_EF=StochasticProgram_Pyomo(objective='obj',first_stage_variables=first_stage_vars,model_builder=model_builder)
+#S_EF.pyo_solver=pyo.SolverFactory('ipopt')
+S_EF.pyo_solver=None
 b='bundle_0'
-EF_model=S_EF.create_EF(scenarios=scenarios,p=p,b=b)
-S_EF.initialize_varmap(b=b,M=EF_model)
-#embed()
-res=S_EF.solve(EF_model,tee=True)
-bundles={'bundle_0':{'ID':['good','bad'],'Probability':[0.5,0.5]},'bundle_1':{'ID':['good','bad'],'Probability':[0.5,0.5]}}
-for b in bundles.keys():
-    S_EF=StochasticProgram_Pyomo(first_stage_variables=first_stage_vars,model_builder=model_builder)
-    S_EF.pyo_solver=pyo.SolverFactory('ipopt')
+bundle_data={"scenarios": [
+    { "ID": 'good',
+        "Fidelity": "HF",
+        "Demand": 2.1,
+        "Weight": 1,
+        "Probability": 0.5
+    },
+    { "ID": 'bad',
+        "Fidelity": "HF",
+        "Demand": 1.2,
+        "Weight": 1,
+        "Probability": 0.5
+    }]}
+
+S_EF.initialize_bundles(bundle_data=bundle_data,bundle_scheme='single_bundle')
+EF_model=S_EF.create_EF(b=list(S_EF.scenarios_in_bundle.keys())[0])
+res=pyo.SolverFactory('ipopt').solve(EF_model,tee=True)
+embed()
+#res=S_EF.solve(EF_model,solver_options={'tee':True})
+#tee=solver_options.get('tee',tee)
+for b in list(S_EF.scenarios_in_bundle.keys()):
     print(b)
-    EF_model=S_EF.create_EF(scenarios=bundles[b]['ID'],p=dict(zip(bundles[b]['ID'],bundles[b]['Probability'])),b=b)
-    S_EF.initialize_varmap(b=b,M=EF_model)
-    res=S_EF.solve(EF_model,tee=True)
+    EF_model=S_EF.create_EF(b=b)
+    res=pyo.SolverFactory('ipopt').solve(EF_model,tee=True)
+
+    for s in S_EF.scenarios_in_bundle[b]:
+        print(pyo.value(EF_model.s[s].x))
+
+
