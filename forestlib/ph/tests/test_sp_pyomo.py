2fd3fc4024c84ffa25c852acd4a0e49217cad242
whart222 <whart222@gmail.com>
2024-11-08 03:45:18 -0700

    Fixing tests

--- forestlib/ph/tests/test_sp_pyomo.py
+++ forestlib/ph/tests/test_sp_pyomo.py
@@ -8,28 +8,33 @@ from forestlib.ph.sp import stochastic_program
 class TestPHPyomo(object):
 
     def test_simple1(self):
-        def builder(**kwargs):
+        def builder(*args, **kwargs):
             M = pyo.ConcreteModel()
             M.x = pyo.Var()
             M.y = pyo.Var()
             M.o = pyo.Objective(expr=M.x)
+            return M
 
-        sp = stochastic_program(
-            objective="o", first_stage_variables=["x"], model_builder=builder
-        )
-        sp.initialize_varmap(b=0, M=M)
+        sp = stochastic_program(first_stage_variables=["x"], model_builder=builder)
+        sp.initialize_bundles(bundle_data=dict(scenarios=[dict(ID=1),dict(ID=2)]))
+        M = sp.create_subproblem("1")
+
         assert sp.varcuid_to_int == {pyo.ComponentUID("x"): 0}
-        assert list(sp.int_to_var[0].keys()) == [0]
+        assert list(sp.int_to_FirstStageVar["1"].keys()) == [0]
         assert sp.shared_variables() == [0]
 
     def test_simple2(self):
-        def builder(**kwargs):
+        def builder(*args, **kwargs):
             M = pyo.ConcreteModel()
             M.x = pyo.Var()
             M.y = pyo.Var()
+            M.o = pyo.Objective(expr=M.x)
+            return M
 
         sp = stochastic_program(first_stage_variables=["y", "x"], model_builder=builder)
-        sp.initialize_varmap(b=0, M=M)
+        sp.initialize_bundles(bundle_data=dict(scenarios=[dict(ID=1),dict(ID=2)]))
+        M = sp.create_subproblem("1")
+
         assert sp.varcuid_to_int == {pyo.ComponentUID("y"): 0, pyo.ComponentUID("x"): 1}
-        assert list(sorted(sp.int_to_var[0].keys())) == [0, 1]
+        assert list(sorted(sp.int_to_FirstStageVar["1"].keys())) == [0, 1]
         assert sp.shared_variables() == [0, 1]
