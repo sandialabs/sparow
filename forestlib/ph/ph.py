7f2a29c4c9de9ad83cf8b5736d1cdc32eccfde5a
whart222 <whart222@gmail.com>
2025-01-10 11:26:18 -0500

    Adding a solution pool.

--- forestlib/ph/ph.py
+++ forestlib/ph/ph.py
@@ -5,12 +5,59 @@ import numpy as np
 
 import logging
 import forestlib.logs
+from forestlib import solnpool
 
 logger = forestlib.logs.logger
 
 
 def norm(values, p):
     return np.linalg.norm(np.array(values), ord=p)
+
+
+def finalize_ph_results(soln, *, sp, soln_pool, finalize_xbar_by_rounding=True):
+    xbar = [soln.variable(i).value for i in range(len(soln.variables()))]
+    assert len(xbar) == len(
+        sp.shared_variables()
+    ), "Mismatch between solution variables and SP model variables: {len(xbar)} != {len(sp.shared_variables())}"
+    #
+    # We use xbar to identify a point that is feasible for all scenarios.
+    #
+    if sp.continuous_fsv():
+        logger.info("Final solution is continuous")
+        #
+        # Evaluate the final xbar, and keep if feasible.
+        #
+        sol = sp.evaluate([xbar[x] for x in sp.shared_variables()])
+        if sol.feasible:
+            soln_pool.add(
+                variables=soln.variables(),
+                objective=solnpool.Objective(value=sol.objective),
+                suffix=soln.suffix,
+            )
+    else:
+        logger.info("Final solution has binary or integer variables")
+
+        if finalize_xbar_by_rounding:
+            #
+            # Round the final xbar, and keep if feasible.
+            #
+            logger.info(
+                "Rounding xbar values associated with binary and integer variables"
+            )
+            tmpx = [sp.round(x, xbar[x]) for x in sp.shared_variables()]
+            sol = sp.evaluate(tmpx)
+            if sol.feasible:
+                solutions.append(sol)
+                variables = copy.copy(soln.variables())
+                for v in variables:
+                    v.value = tmpx[v.index]
+                soln_pool.add(
+                    variables=variables,
+                    objective=solnpool.Objective(value=sol.objective),
+                    suffix=soln.suffix,
+                )
+
+    return soln_pool
 
 
 class ProgressiveHedgingSolver(object):
@@ -25,6 +72,8 @@ class ProgressiveHedgingSolver(object):
         self.solver_name = None
         self.solver_options = {}
         self.finalize_xbar_by_rounding = True
+        self.finalize_all_xbar = False
+        self.solution_pool = None
 
     def set_options(
         self,
@@ -39,6 +88,8 @@ class ProgressiveHedgingSolver(object):
         solver_options=None,
         loglevel=None,
         finalize_xbar_by_rounding=None,
+        finalize_all_xbar=None,
+        solution_pool=None,
     ):
         #
         # Misc configuration
@@ -61,6 +112,10 @@ class ProgressiveHedgingSolver(object):
             self.solver_options = solver_options
         if finalize_xbar_by_rounding is not None:
             self.finalize_xbar_by_rounding = finalize_xbar_by_rounding
+        if finalize_all_xbar is not None:
+            self.finalize_all_xbar = finalize_all_xbar
+        if solution_pool is not None:
+            self.solution_pool = solution_pool
 
         if loglevel is not None:
             if loglevel == "DEBUG":
@@ -76,11 +131,33 @@ class ProgressiveHedgingSolver(object):
             print(f"  convergence_norm           {self.convergence_norm}")
             print(f"  convergence_tolerance      {self.convergence_tolerance}")
             print(f"  finalize_xbar_by_rounding  {self.finalize_xbar_by_rounding}")
+            print(f"  finalize_all_xbar          {self.finalize_all_xbar}")
             print(f"  max_iterations             {self.max_iterations}")
             print(f"  normalize_convergence_norm {self.normalize_convergence_norm}")
             print(f"  rho                        {self.rho}")
             print(f"  solver_name                {self.solver_name}")
             print("")
+
+        #
+        # Setup solution pool and archive context information
+        #
+        # We disable hashing of variables to ensure we keep the solution for each iteration of PH.
+        #
+        if self.solution_pool is None:
+            self.solution_pool = solnpool.SolutionPool()
+        sp_metadata = self.solution_pool.set_context(
+            "PH Iterations", hash_variables=False
+        )
+        sp_metadata.solver = "PH Iteration Results"
+        sp_metadata.solver_options = dict(
+            rho=self.rho,
+            cached_model_generation=self.cached_model_generation,
+            max_iterations=self.max_iterations,
+            convergence_tolerance=self.convergence_tolerance,
+            normalize_convergence_norm=self.normalize_convergence_norm,
+            solver_name=self.solver_name,
+            solver_options=self.solver_options,
+        )
 
         # The StochProgram object manages the sub-solver interface.  By default, we assume
         #   the user has initialized the sub-solver within the SP object.
@@ -124,16 +201,16 @@ class ProgressiveHedgingSolver(object):
             for x in sfs_variables:
                 w[b][x] = self.rho * (sp.get_variable_value(b, x) - xbar[x])
 
+        # Step 4.1
         iteration = 0
         termination_condition = "Termination: unknown"
+        latest_soln = self.archive_solution(
+            sp=sp, xbar=xbar, w=w, iteration=iteration, obj_lb=obj_lb
+        )
+        self.log_iteration(iteration=iteration, obj_lb=obj_lb, xbar=xbar, rho=self.rho)
+
         while True:
-            logger.info("")
-            logger.info("-" * 70)
-            logger.info(f"Iteration:    {iteration}")
-            logger.info(f"obj_lb:      {obj_lb}")
-            logger.verbose(f"xbar:        {xbar}")
-            logger.verbose(f"rho:         {self.rho}")
-            logger.info("")
+            iteration += 1
 
             # Step 5
             xbar_prev = xbar
@@ -192,13 +269,20 @@ class ProgressiveHedgingSolver(object):
                 g /= len(sfs_variables)
             logger.info(f"g = {g}")
 
+            # Step 9.1
+            latest_soln = self.archive_solution(
+                sp=sp, xbar=xbar, w=w, iteration=iteration, obj_lb=obj_lb, g=g
+            )
+            self.log_iteration(
+                iteration=iteration, obj_lb=obj_lb, xbar=xbar, rho=self.rho
+            )
+
             # Step 10
             if g < self.convergence_tolerance:
                 termination_condition = f"Termination: convergence tolerance ({g} < {self.convergence_tolerance})"
                 logger.info(termination_condition)
                 break
 
-            iteration += 1
             if iteration >= self.max_iterations:
                 termination_condition = f"Termination: max_iterations ({iteration} == {self.max_iterations})"
                 logger.info(termination_condition)
@@ -206,71 +290,61 @@ class ProgressiveHedgingSolver(object):
 
             self.update_rho(iteration)
 
+        sp_metadata = self.solution_pool.metadata
+        sp_metadata.iterations = iteration
+        sp_metadata.termination_condition = termination_condition
+
         logger.info("")
         logger.info("-" * 70)
         logger.info("ProgressiveHedgingSolver - FINALIZING")
-        results = self.finalize_results(
-            sp,
-            xbar=xbar,
-            w=w,
-            g=g,
-            iterations=iteration,
-            termination_condition=termination_condition,
-            obj_lb=obj_lb,
-        )
+        if self.finalize_all_xbar:
+            all_iterations = self.solution_pool.solutions
+            # NOTE: we disable hashing here because we want to keep solutions for each iteration
+            self.solution_pool.set_context(
+                "Finalized All PH Iterations", hash_variables=False
+            )
+            for soln in all_iterations:
+                finalize_ph_results(soln, sp=sp, soln_pool=self.solution_pool)
+        else:
+            soln = self.solution_pool[latest_soln]
+            self.solution_pool.set_context("Finalized Last PH Solution")
+            finalize_ph_results(soln, sp=sp, soln_pool=self.solution_pool)
+
         logger.info("")
         logger.info("-" * 70)
         logger.info("ProgressiveHedgingSolver - RESULTS")
         if logger.level != logging.NOTSET and logger.level <= logging.VERBOSE:
-            pprint.pprint(results.toDict())
+            pprint.pprint(self.solution_pool.to_dict())
             sys.stdout.flush()
 
         logger.info("")
         logger.info("-" * 70)
         logger.info("ProgressiveHedgingSolver - STOP")
 
-        return results
+        return self.solution_pool
+
+    def log_iteration(self, **kwds):
+        logger.info("")
+        logger.info("-" * 70)
+        logger.info(f"Iteration:   {kwds['iteration']}")
+        logger.info(f"obj_lb:      {kwds['obj_lb']}")
+        logger.verbose(f"xbar:        {kwds['xbar']}")
+        logger.verbose(f"rho:         {kwds['rho']}")
+        logger.info("")
+
+    def archive_solution(self, *, sp, xbar=None, w=None, **kwds):
+        b = next(iter(sp.bundles))
+        variables = [
+            solnpool.Variable(
+                value=val,
+                index=i,
+                name=sp.get_variable_name(b, i),
+                suffix=munch.Munch(w={k: v[i] for k, v in w.items()}),
+            )
+            for i, val in xbar.items()
+        ]
+        return self.solution_pool.add(variables=variables, **kwds)
 
     def update_rho(self, iteration):
         # TODO HERE
         pass
-
-    def finalize_results(
-        self, sp, *, xbar, w, g, iterations, termination_condition, obj_lb
-    ):
-        #
-        # We use xbar to identify a point that is feasible for all scenarios.
-        #
-        solutions = []
-        if sp.continuous_fsv():
-            logger.info("Final solution is continuous")
-            #
-            # Evaluate the final xbar, and keep if feasible.
-            #
-            sol = sp.evaluate([xbar[x] for x in sp.shared_variables()])
-            if sol.feasible:
-                solutions.append(sol)
-        else:
-            logger.info("Final solution has binary or integer variables")
-
-            if self.finalize_xbar_by_rounding:
-                #
-                # Round the final xbar, and keep if feasible.
-                #
-                logger.info(
-                    "Rounding xbar values associated with binary and integer variables"
-                )
-                tmpx = [sp.round(x, xbar[x]) for x in sp.shared_variables()]
-                sol = sp.evaluate(tmpx)
-                if sol.feasible:
-                    solutions.append(sol)
-
-        return munch.Munch(
-            xbar=xbar,
-            w=w,
-            g=g,
-            iterations=iterations,
-            termination_condition=termination_condition,
-            obj_lb=obj_lb,
-            solutions=solutions,
-        )
--- forestlib/solnpool.py
+++ forestlib/solnpool.py
@@ -5,8 +5,25 @@ import munch
 nan = float("nan")
 
 
+class MyMunch(munch.Munch):
+
+    to_dict = munch.Munch.toDict
+
+
+def _to_dict(x):
+    xtype = type(x)
+    if xtype in [float, int, complex, str, list, bool] or x is None:
+        return x
+    elif xtype in [tuple, set, frozenset]:
+        return list(x)
+    elif xtype in [dict, munch.Munch, MyMunch]:
+        return {k: _to_dict(v) for k, v in x.items()}
+    else:
+        return x.to_dict()
+
+
 def _custom_dict_factory(data):
-    return {k: v.toDict() if type(v) is munch.Munch else v for k, v in data}
+    return {k: _to_dict(v) for k, v in data}
 
 
 @dataclasses.dataclass
@@ -18,10 +35,10 @@ class Variable:
     repn = None
     index: int = None
     discrete: bool = False
-    suffix: munch.Munch = dataclasses.field(default_factory=munch.Munch)
+    suffix: MyMunch = dataclasses.field(default_factory=MyMunch)
 
     def to_dict(self):
-        return dataclasses.asdict(self, dict_factory=custom_dict_factory)
+        return dataclasses.asdict(self, dict_factory=_custom_dict_factory)
 
 
 @dataclasses.dataclass
@@ -29,10 +46,10 @@ class Objective:
     _: dataclasses.KW_ONLY
     value: float = nan
     name: str = None
-    suffix: munch.Munch = dataclasses.field(default_factory=munch.Munch)
+    suffix: MyMunch = dataclasses.field(default_factory=MyMunch)
 
     def to_dict(self):
-        return dataclasses.asdict(self, dict_factory=custom_dict_factory)
+        return dataclasses.asdict(self, dict_factory=_custom_dict_factory)
 
 
 class Solution:
@@ -41,7 +58,7 @@ class Solution:
 
     def __init__(self, *, variables=None, objectives=None, **kwds):
         self.id = self._id_counter
-        self._id_counter += 1
+        Solution._id_counter += 1
 
         self._variables = []
         self.int_to_variable = {}
@@ -58,11 +75,15 @@ class Solution:
         self.str_to_objective = {}
         if objectives is not None:
             self._objectives = objectives
-            for o in objectives:
-                if o.name is not None:
-                    self.str_to_objective[o.name] = o
+        elif "objective" in kwds:
+            self._objectives = [kwds.pop("objective")]
+        for o in self._objectives:
+            self.str_to_objective[o.name] = o
 
-        self.suffix = munch.Munch(**kwds)
+        if "suffix" in kwds:
+            self.suffix = MyMunch(kwds.pop("suffix"))
+        else:
+            self.suffix = MyMunch(**kwds)
 
     def variable(self, index):
         if type(index) is int:
@@ -73,7 +94,19 @@ class Solution:
     def variables(self):
         return self._variables
 
-    def objective(self, index):
+    def tuple_repn(self):
+        if len(self.int_to_variable) == len(self._variables):
+            return tuple(
+                tuple([k, var.value]) for k, var in self.int_to_variable.items()
+            )
+        elif len(self.str_to_variable) == len(self._variables):
+            return tuple(
+                tuple([k, var.value]) for k, var in self.str_to_variable.items()
+            )
+        else:
+            return tuple(tuple([k, var.value]) for k, var in enumerate(self._variables))
+
+    def objective(self, index=None):
         if type(index) is int:
             return self.int_to_objective[index]
         else:
@@ -84,25 +117,56 @@ class Solution:
 
     def to_dict(self):
         return dict(
+            id=self.id,
             variables=[v.to_dict() for v in self.variables()],
-            objectives=[v.to_dict() for o in self.objectives()],
+            objectives=[o.to_dict() for o in self.objectives()],
+            suffix=self.suffix.to_dict(),
         )
 
 
 class SolutionPool:
 
     def __init__(self):
-        self._context = "none"
-        self.solutions = {"none":{}}
+        self._context_name = "none"
+        self._solutions = {}
+        self.set_context(self._context_name)
 
     @property
-    def context(self):
-        return self._context
+    def metadata(self):
+        return self._solutions[self._context_name].context
 
-    def set_context(self, context):
-        if context not in self.solutions:
-            self.solutions[context] = {}
-        self._context = context
+    @property
+    def solutions(self):
+        return self._solutions[self._context_name].solutions.values()
+
+    @property
+    def last_solution(self):
+        context = self._solutions[self._context_name]
+        index = next(reversed(context.solutions.keys()))
+        return context.solutions[index]
+
+    def __iter__(self):
+        for soln in self._solutions[self._context_name].solutions.values():
+            yield soln
+
+    def __len__(self):
+        return len(self._solutions[self._context_name].solutions)
+
+    def __getitem__(self, soln_id, context_name=None):
+        if context_name is None:
+            context_name = self._context_name
+        return self._solutions[context_name].solutions[soln_id]
+
+    def set_context(self, name, *, hash_variables=True):
+        if name not in self._solutions:
+            self._solutions[name] = MyMunch(
+                context=MyMunch(context_name=name),
+                solutions={},
+                hash_variables=hash_variables,
+                unique_solutions=set(),
+            )
+        self._context_name = name
+        return self.metadata
 
     def add(self, *args, **kwargs):
         if len(args) == 1 and len(kwargs) == 0:
@@ -110,10 +174,23 @@ class SolutionPool:
             soln = args[0]
         else:
             soln = Solution(*args, **kwargs)
-        self.solutions[self._context][soln.id] = soln
+        #
+        # Return None if hash_variables is True and the solution has already been added to the pool
+        #
+        context = self._solutions[self._context_name]
+        if context.hash_variables:
+            tuple_repn = soln.tuple_repn()
+            if tuple_repn in context.unique_solutions:
+                return None
+
+        assert (
+            soln.id not in context.solutions
+        ), f"Solution id {soln.id} already in solution pool context '{self._context_name}'"
+        context.solutions[soln.id] = soln
+        return soln.id
 
     def to_dict(self):
-        return {k:{i:soln.to_dict() for i,soln in v.items()} for k,v in self.solutions.items()}
+        return _to_dict(self._solutions)
 
     def write(self, json_filename, indent=None):
         with open(json_filename, "w") as OUTPUT:
@@ -128,5 +205,4 @@ class SolutionPool:
                 data = json.load(INPUT)
             except ValueError as e:
                 raise ValueError(f"Invalid JSON in file '{json_filename}': {e}")
-            self.solutions = data.solutions
-
+            self._solutions = data.solutions
