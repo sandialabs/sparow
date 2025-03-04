fedb16aaf2a1725cdcb9ac41b872624c384cc9cd
Rachael May Alfant <rmalfan@sisu3.sandia.gov>
2025-03-04 15:33:13 -0700

    restoring working version of farmers

--- examples/farmers/MFfarmers.py
+++ examples/farmers/MFfarmers.py
@@ -736,28 +736,6 @@ def HF_PH(*, cache, max_iter, loglevel, finalize_all_iters):
     results.write("results.json", indent=4)
     print("Writing results to 'results.json'")
 
-def dist_map(data, models):
-    model0 = models[0]
-    
-    HFscenarios = list(data[model0].keys())
-    LFscenarios = {}  # all other models are LF
-    for model in models[1:]:
-        LFscenarios[model] = list(data[model].keys())
-
-    print(data[model0])
-
-    HFdemands = list(data[model0][HFkey]["Yield"] for HFkey in HFscenarios)
-    LFdemands = list(data[model][ls]["Yield"] for ls in LFscenarios[model] for model in models[1:])
-
-    # map each LF scenario to closest HF scenario using 1-norm of demand difference
-    demand_diffs = {}
-    for i in range(len(HFdemands)):
-        for j in range(len(LFdemands)):
-            demand_diffs[(i,j)] = sum(abs(HFdemands[i][ind] - LFdemands[j][ind]) for ind in range(len(HFdemands[i])))
-
-    return demand_diffs
-
-
 def MF_PH(*, cache, max_iter, loglevel, finalize_all_iters):
     print("-" * 60)
     print("Running MF_PH")
@@ -776,8 +754,7 @@ def MF_PH(*, cache, max_iter, loglevel, finalize_all_iters):
 
     bundle_num = 0
     sp.initialize_bundles(
-        scheme="similar_partitions",
-        distance_function=dist_map,
+        scheme="mf_random_nested",
         LF=2,
         seed=1234567890,
         model_weight={"HF": 2.0, "LF": 1.0},
