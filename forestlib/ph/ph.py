0e7b460b245b4840de27384a6bf52e33ad044b35
whart222 <whart222@gmail.com>
2025-10-23 11:53:10 -0600

    Reworking sums into more compact forms

--- forestlib/ph/ph.py
+++ forestlib/ph/ph.py
@@ -205,9 +205,10 @@ class ProgressiveHedgingSolver(object):
         # Step 3
         xbar = {}
         for x in sfs_variables:
-            xbar[x] = 0.0
-            for b in sp.bundles:
-                xbar[x] += sp.bundles[b].probability * sp.get_variable_value(b, x)
+            xbar[x] = sum(
+                sp.bundles[b].probability * sp.get_variable_value(b, x)
+                for b in sp.bundles
+            )
         self.update_rho(sfs_variables, xbar, sp)
 
         # Step 4
@@ -281,13 +282,16 @@ class ProgressiveHedgingSolver(object):
             # Step 7
             xbar = {}
             for x in sfs_variables:
-                xbar[x] = 0.0
+                xbar[x] = sum(
+                    sp.bundles[b].probability * sp.get_variable_value(b, x)
+                    for b in sp.bundles
+                )
                 for b in sp.bundles:
                     logger.debug(
                         f"Variable: {x} {b} {sp.get_variable_name(x)} {sp.get_variable_value(b, x)}"
                     )
-                    xbar[x] += sp.bundles[b].probability * sp.get_variable_value(b, x)
             logger.debug(f"xbar = {xbar}")
+
             self.update_rho(sfs_variables, xbar, sp)
             logger.debug(f"rho = {self.rho}")
 
@@ -303,20 +307,23 @@ class ProgressiveHedgingSolver(object):
 
             # Step 9
             #
-            # NOTE: We use xbar_prev instead of xbar to compute 'g'.  If you use xbar, then
-            #       all subproblems could have hte same value, but in the *next* iteration they
+            # NOTE: Should we use xbar_prev instead of xbar to compute 'g'?  If you use xbar, then
+            #       all subproblems could have the same value, but in the *next* iteration they
             #       subproblems could have different values.  We need to assess the difference
             #       between the xbar that generated the current values.
             #
-            g = 0.0
-            for b in sp.bundles:
-                g += sp.bundles[b].probability * norm(
+            g = sum(
+                sp.bundles[b].probability
+                * norm(
                     [sp.get_variable_value(b, x) - xbar[x] for x in sfs_variables],
                     self.convergence_norm,
                 )
+                for b in sp.bundles
+            )
             if self.normalize_convergence_norm:
                 g /= len(sfs_variables)
             logger.info(f"g = {g}")
+
             G = norm(
                 [xbar[x] - xbar_prev[x] for x in sfs_variables], self.convergence_norm
             )
