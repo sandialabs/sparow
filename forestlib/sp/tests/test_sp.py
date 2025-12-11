9ce9585d898a026561954bb29f80ea1a3c4b4be3
whart222 <whart222@gmail.com>
2025-12-11 16:45:28 -0500

    Misc cleanup and adding test for variable names

--- forestlib/sp/tests/test_sp.py
+++ forestlib/sp/tests/test_sp.py
@@ -7,6 +7,7 @@ from forestlib.sp import stochastic_program
 # TODO - Remove the extraneous 'd' data in the model_data after fixing
 #        bundling bugs associated with this.
 
+
 @pytest.fixture
 def sp0():
     def builder(*args, **kwargs):
@@ -52,6 +53,22 @@ def sp2():
     return sp
 
 
+@pytest.fixture
+def sp3():
+    def builder(*args, **kwargs):
+        M = pyo.ConcreteModel()
+        M.x = pyo.Var()
+        M.b = pyo.Block()
+        M.b.y = pyo.Var(within=pyo.Binary)
+        M.o = pyo.Objective(expr=M.x)
+        return M
+
+    sp = stochastic_program(first_stage_variables=["b.y", "x"])
+    sp.initialize_model(model_builder=builder)
+    sp.initialize_model(model_data=dict(scenarios=[dict(ID=1, d=0), dict(ID=2, d=0)]))
+    return sp
+
+
 class TestSP(object):
 
     def test_simple0(self, sp0):
@@ -61,6 +78,7 @@ class TestSP(object):
         assert list(sp0.int_to_FirstStageVar["1"].keys()) == [0]
         assert sp0.shared_variables() == [0]
         assert sp0.get_objective_coef(0) == 1.0  # x
+        assert sp0.get_variable_name(0) == "x"
 
     def test_simple1(self, sp1):
         M = sp1.create_subproblem("1")
@@ -69,14 +87,7 @@ class TestSP(object):
         assert list(sp1.int_to_FirstStageVar["1"].keys()) == [0]
         assert sp1.shared_variables() == [0]
         assert sp1.get_objective_coef(0) == 0.5  # x
-
-    def test_simple1(self, sp1):
-        M = sp1.create_subproblem("1")
-
-        assert sp1.varcuid_to_int == {pyo.ComponentUID("x"): 0}
-        assert list(sp1.int_to_FirstStageVar["1"].keys()) == [0]
-        assert sp1.shared_variables() == [0]
-        assert sp1.get_objective_coef(0) == 0.5  # x
+        assert sp1.get_variable_name(0) == "x"
 
     def test_simple2(self, sp2):
         M = sp2.create_subproblem("1")
@@ -89,6 +100,22 @@ class TestSP(object):
         assert sp2.shared_variables() == [0, 1]
         assert sp2.get_objective_coef(0) == 0  # y
         assert sp2.get_objective_coef(1) == 0.5  # x
+        assert sp2.get_variable_name(0) == "y"
+        assert sp2.get_variable_name(1) == "x"
+
+    def test_simple3(self, sp3):
+        M = sp3.create_subproblem("1")
+
+        assert sp3.varcuid_to_int == {
+            pyo.ComponentUID("b.y"): 0,
+            pyo.ComponentUID("x"): 1,
+        }
+        assert list(sorted(sp3.int_to_FirstStageVar["1"].keys())) == [0, 1]
+        assert sp3.shared_variables() == [0, 1]
+        assert sp3.get_objective_coef(0) == 0  # y
+        assert sp3.get_objective_coef(1) == 0.5  # x
+        assert sp3.get_variable_name(0) == "b.y"
+        assert sp3.get_variable_name(1) == "x"
 
     def test_continuous1(self, sp1):
         M = sp1.create_subproblem("1")
