1bec8cda6d1b7315d3934a4a6a6d6cc7794f4e69
Rachael May Alfant <rmalfan@sandia.gov>
2026-03-31 16:33:24 -0600

    merging in changes from main

--- examples/mpisppy/driver.py
+++ examples/mpisppy/driver.py
@@ -2,22 +2,23 @@ import sys
 import pprint
 import mpisppy
 #from sparow_examples.pmedian.pmedian import *
-from sparow_examples.facilityloc.facilityloc import *
+from sparow_examples.facilityloc.grid_facilityloc import *
 from sparow.ph import ProgressiveHedgingSolver
 #from sparow.ef import ExtensiveFormSolver
 from sparow.ph.ph_mpisppy import ProgressiveHedgingSolver_MPISPPY
 
-sp = LF_facilityloc()
-solver = ProgressiveHedgingSolver() #ProgressiveHedgingSolver_MPISPPY()
+sp = random_HF_LF1_grid_facilityloc()
+solver = ProgressiveHedgingSolver_MPISPPY() #ProgressiveHedgingSolver()
 solver.set_options(
     solver="gurobi",
     max_iterations=100,
     rho_updates=True,
-    loglevel="INFO"
+    loglevel="INFO",
+    mpisppy_options=["--lagrangian", "--xhatshuffle", "--rel-gap=0.01"]
 )
 
 results_mpi = solver.solve(sp, solver="gurobi")
-pprint.pprint(results_mpi.to_dict())
+#pprint.pprint(results_mpi.to_dict())
 if getattr(solver, "mpi_rank", 0) == 0:
     pprint.pprint(results_mpi.to_dict())
     #results_mpi.write("results_mpi.json", indent=4)
--- sparow/ph/ph_mpisppy.py
+++ sparow/ph/ph_mpisppy.py
@@ -17,6 +17,7 @@ with or_topas.util.try_import() as mpisppy_available:
 
 from sparow.sp.sp_pyomo import find_objective
 import sparow.logs
+from sparow import solnpool
 
 logger = sparow.logs.logger
 
@@ -378,7 +379,7 @@ class ProgressiveHedgingSolver_MPISPPY(object):
         #
         if self.mpi_rank == 0:
             if self.solutions is None:
-                self.solutions = or_topas.solnpool.PoolManager()
+                self.solutions = solnpool.SparowPoolManager()
             if self.finalize_all_xbar:
                 sp_metadata = self.solutions.add_pool(
                     name="PH Iterations", policy=or_topas.solnpool.PoolPolicy.keep_all
@@ -439,7 +440,7 @@ class ProgressiveHedgingSolver_MPISPPY(object):
             for soln in results["first_stage_solutions"]:
                 args = dict(sp=sp, xbar=soln)
                 if results["best_value"]:
-                    args["objective"] = or_topas.solnpool.ObjectiveInfo(
+                    args["objective"] = solnpool.create_objective(
                         value=float(results["best_value"])
                     )
                 self.archive_solution(**args)
@@ -463,7 +464,7 @@ class ProgressiveHedgingSolver_MPISPPY(object):
     def archive_solution(self, *, sp, xbar, w=None, **kwds):
         w = {} if w is None else w
         variables = [
-            or_topas.solnpool.VariableInfo(
+            solnpool.create_variable(
                 value=float(val),
                 index=i,
                 name=sp.get_variable_name(i),
