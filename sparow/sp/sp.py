5fed1d91591dbefaa9b097942c36157f20292eed
whart222 <whart222@gmail.com>
2026-04-01 09:04:56 -0600

    Adding timing logic

--- sparow/sp/sp.py
+++ sparow/sp/sp.py
@@ -6,6 +6,7 @@ import munch
 import logging
 import functools
 from .bundling import bundling_functions
+from pyomo.common.timing import tic, toc
 
 # import sparow.util
 import sparow.logs
@@ -82,12 +83,17 @@ class StochasticProgram(object):
         # solutions
         self.default_model = None
 
+        self._timing = {}
+
     def initialize_application(self, *, filename=None, app_data=None, **kwargs):
         if filename is not None:
             with open(f"{filename}", "r") as file:
                 self.app_data = json.load(filename)
         elif app_data is not None:
             self.app_data = app_data
+
+    def get_timing(self, model):
+        return self._timing.get(id(model), dict())
 
     # DEPRECATED METHOD(?)
     def initialize_bundles(self, *, scheme=None, models=None, **kwargs):
@@ -158,11 +164,18 @@ class StochasticProgram(object):
     def create_subproblem(
         self, b, *, w=None, x_bar=None, rho=None, cached=False, compact_repn=True
     ):
+        tic(None)
         model = self.create_bundle_EF(
             b=b, w=w, x_bar=x_bar, rho=rho, cached=cached, compact_repn=compact_repn
         )
+        construction_time = toc(None)
+
+        tic(None)
         if hasattr(self,'transform_subproblem'):
             model = self.transform_subproblem(self,model)
+        transformation_times = toc(None)
+
+        self._timing[id(model)] = dict(transformations=transformation_times, construction=construction_time)
         
         return model
 
