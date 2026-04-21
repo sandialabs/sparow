8350b37c03de85c226b3194b1d08c2639e949ee0
whart222 <whart222@gmail.com>
2026-04-21 07:53:14 -0600

    Moving initialize_bundles

--- sparow/sp/sp.py
+++ sparow/sp/sp.py
@@ -5,62 +5,12 @@ import copy
 import munch
 import logging
 
-from .bundling import bundling_functions
+from .bundling import initialize_bundles
 
 # import sparow.util
 import sparow.logs
 
 logger = sparow.logs.logger
-
-
-def initialize_bundles(
-    *,
-    scheme=None,
-    models=None,
-    default_model=None,
-    model_data=None,
-    scenario_data=None,
-    **kwargs,
-):
-    if scenario_data is None:
-        scenario_data = {}
-    if model_data is None:
-        model_data = {}
-
-    if scheme == None:
-        scheme = "single_scenario"
-    if models == None:
-        models = [default_model] + list(
-            sorted(model for model in scenario_data.keys() if model != default_model)
-        )
-    else:
-        for name in models:
-            assert name in scenario_data
-
-    assert len(models) > 0, "Cannot initialize bundles without model data"
-    if "model_weight" in kwargs:
-        model_weight = kwargs["model_weight"]
-    else:
-        model_weight = {
-            model: mdata.get("_model_weight_", 1.0)
-            for model, mdata in model_data.items()
-        }
-
-    if model_weight:
-        return bundling_functions.BundleObj(
-            data=scenario_data,
-            models=models,
-            model_weight=model_weight,
-            scheme=scheme,
-            bundle_args=kwargs,
-        )
-    else:
-        return bundling_functions.BundleObj(
-            data=scenario_data,
-            models=models,
-            scheme=scheme,
-            bundle_args=kwargs,
-        )
 
 
 class StochasticProgram(object):
