67ec977efe6184d16f6e37c03ad821e80287bb34
whart222 <whart222@gmail.com>
2026-06-23 10:02:59 -0600

    Adding an alternate driver using newsvendor

---
+++ doc/how_to_notebooks/mpi_driver.py
@@ -0,0 +1,37 @@
+# import parallel PH solver (mpi-sppy wrapper)
+from sparow.ph.ph_mpisppy import ProgressiveHedgingSolver_MPISPPY
+
+# import newsvendor exemple from sparow
+from sparow.sp.examples import simple_newsvendor
+
+# SP model object
+example = simple_newsvendor()
+
+# solving in parallel with the sparow wrapper around mpisppy
+solver = ProgressiveHedgingSolver_MPISPPY()
+
+solver.set_options(
+    # solving with highs
+    solver="highs",
+    # this will default to 100
+    max_iterations=2,
+    # can replace with DEBUG, VERBOSE, etc.
+    loglevel="INFO",
+    # rho by default will already be 1.5
+    default_rho=1.5,
+    # mpisppy specific options
+    mpisppy_options=[
+        "--tee-rank0-solves",
+        "--lagrangian",
+        "--xhatshuffle",
+        "--rel-gap=0.01",
+    ],
+)
+
+# Call mpisppy and return a sparow solution object
+results_mpi = solver.solve(example.sp)
+
+
+if getattr(solver, "mpi_rank", 0) == 0:
+    # Print results on Rank 0
+    pprint.pprint(results_mpi.to_dict())
